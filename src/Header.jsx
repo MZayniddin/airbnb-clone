689b1c346684110f2447fa5727d8c0a0d06154ae
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-06-25 14:51:45 +0500

    create account page

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./userContext";
 
 const Header = () => {
+  const { user } = useContext(UserContext);
   return (
     <header className="flex justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to="/" className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -18,8 +21,8 @@ const Header = () => {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold">aribnb</span>
-      </a>
+        <span className="font-bold">airbnb</span>
+      </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
@@ -44,7 +47,7 @@ const Header = () => {
         </button>
       </div>
       <Link
-        to="/login"
+        to={user ? "account" : "/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
@@ -75,6 +78,7 @@ const Header = () => {
             />
           </svg>
         </div>
+        {!!user && <div>{user.name}</div>}
       </Link>
     </header>
   );
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (ev) => {
+    ev.preventDefault();
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Registration successfull. Now you can log in");
+    } catch (error) {
+        alert("Registration failed. Please try again later")
+    }
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mt-8">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            autoComplete="current-password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already? a member?{" "}
             <Link className="underline text-black" to="/login">
